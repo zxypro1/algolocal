7a88a734f62b7db890434ad4f551f5f89cb8e338
Zone Tome <1018995004@qq.com>
2026-08-12 22:46:14 +0800

    chore: release v0.3.0

--- electron-builder.config.js
+++ electron-builder.config.js
@@ -143,7 +143,7 @@ module.exports = {
     entitlements: 'build/entitlements.mac.plist',
     entitlementsInherit: 'build/entitlements.mac.plist',
     bundleVersion: '1',
-    bundleShortVersion: '0.2.1',
+    bundleShortVersion: '0.3.0',
     fileAssociations: [
       {
         ext: 'algo',
